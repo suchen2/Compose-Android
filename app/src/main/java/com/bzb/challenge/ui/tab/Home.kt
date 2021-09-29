e3b31210a353d8f7a8e0805d73a52bb2d956a0b5
bzb <bzb2419@163.com>
2021-09-29 16:27:28 +0800

    去掉侧滑页布局背景

--- app/src/main/java/com/bzb/challenge/ui/tab/Home.kt
+++ app/src/main/java/com/bzb/challenge/ui/tab/Home.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -49,6 +50,7 @@ fun HomePage(scaffoldState: ScaffoldState) {
                 action = { ActionView() }
             )
         },
+        drawerBackgroundColor = Color.Transparent,
         drawerContent = {
             SlidePageView(scaffoldState)
         },
